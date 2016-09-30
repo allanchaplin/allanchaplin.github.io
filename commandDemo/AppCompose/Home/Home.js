cfe3504cd1fc4ba6f34ba2b60fe50ccd6223543c
Allan Chaplin <a.chaplin@nittygritty.net>
2016-09-30 14:15:13 +0100

    Added ajax call.

--- commandDemo/AppCompose/Home/Home.js
+++ commandDemo/AppCompose/Home/Home.js
@@ -17,12 +17,12 @@
       $('#insertCustom').click(insertCustom);
     });
   };
-  
+
   function insertText(textToInsert) {
     // Insert as plain text (CoercionType.Text)
     Office.context.mailbox.item.body.setSelectedDataAsync(
-      textToInsert, 
-      { coercionType: Office.CoercionType.Text }, 
+      textToInsert,
+      { coercionType: Office.CoercionType.Text },
       function (asyncResult) {
         // Display the result to the user
         if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
@@ -37,42 +37,54 @@
   function insertDefault() {
     insertText("Inserted by the Add-in Command Demo add-in.");
   }
-  
+
   function insertMsg1() {
-    insertText("Hello World!");
+    $.ajax({
+      url: "https://support.nittygritty.net/sessions.json",
+      type: "POST",
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function (data) {
+        app.showNotification('Error: ' + errorCode + ' - ' + errorMessage);
+      },
+      error: function (data, errorCode, errorMessage) {
+        app.showNotification('Error: ' + errorCode + ' - ' + errorMessage);
+      }
+    });
+    insertText("Called the scary internet!");
   }
-  
+
   function insertMsg2() {
     insertText("Add-in commands are cool!");
   }
-  
+
   function insertMsg3() {
     insertText("Visit https://dev.outlook.com today for all of your add-in development needs.");
   }
-  
+
   function insertCustom() {
     var textToInsert = $('#textToInsert').val();
     insertText(textToInsert);
   }
 })();
 
-// MIT License: 
- 
-// Permission is hereby granted, free of charge, to any person obtaining 
-// a copy of this software and associated documentation files (the 
-// ""Software""), to deal in the Software without restriction, including 
-// without limitation the rights to use, copy, modify, merge, publish, 
-// distribute, sublicense, and/or sell copies of the Software, and to 
-// permit persons to whom the Software is furnished to do so, subject to 
-// the following conditions: 
- 
-// The above copyright notice and this permission notice shall be 
-// included in all copies or substantial portions of the Software. 
- 
-// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, 
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// MIT License:
+
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// ""Software""), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
