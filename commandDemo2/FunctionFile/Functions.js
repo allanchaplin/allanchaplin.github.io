0e07cb533b9a869de7a5c07e756d7ef85d7e8ab8
Allan Chaplin <a.chaplin@nittygritty.net>
2016-10-03 16:42:17 +0100

    Updated the texts

--- commandDemo2/FunctionFile/Functions.js
+++ commandDemo2/FunctionFile/Functions.js
@@ -17,7 +17,7 @@ function statusUpdate(icon, text) {
 // Adds text into the body of the item, then reports the results
 // to the info bar.
 function addTextToBody(text, icon, event) {
-  Office.context.mailbox.item.body.setSelectedDataAsync(text, { coercionType: Office.CoercionType.Text }, 
+  Office.context.mailbox.item.body.setSelectedDataAsync(text, { coercionType: Office.CoercionType.Text },
     function (asyncResult){
       if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
         statusUpdate(icon, "\"" + text + "\" inserted successfully.");
@@ -37,7 +37,7 @@ function addDefaultMsgToBody(event) {
 }
 
 function addMsg1ToBody(event) {
-  addTextToBody("Hello World!", "red-icon-16", event);
+  addTextToBody("Call the scary internet", "red-icon-16", event);
 }
 
 function addMsg2ToBody(event) {
@@ -51,78 +51,78 @@ function addMsg3ToBody(event) {
 // Gets the subject of the item and displays it in the info bar.
 function getSubject(event) {
   var subject = Office.context.mailbox.item.subject;
-  
+
   Office.context.mailbox.item.notificationMessages.addAsync("subject", {
     type: "informationalMessage",
     icon: "blue-icon-16",
     message: "Subject: " + subject,
     persistent: false
   });
-  
+
   event.completed();
 }
 
 // Gets the item class of the item and displays it in the info bar.
 function getItemClass(event) {
   var itemClass = Office.context.mailbox.item.itemClass;
-  
+
   Office.context.mailbox.item.notificationMessages.addAsync("itemClass", {
     type: "informationalMessage",
     icon: "red-icon-16",
     message: "Item Class: " + itemClass,
     persistent: false
   });
-  
+
   event.completed();
 }
 
 // Gets the date and time when the item was created and displays it in the info bar.
 function getDateTimeCreated(event) {
   var dateTimeCreated = Office.context.mailbox.item.dateTimeCreated;
-  
+
   Office.context.mailbox.item.notificationMessages.addAsync("dateTimeCreated", {
     type: "informationalMessage",
     icon: "red-icon-16",
     message: "Created: " + dateTimeCreated.toLocaleString(),
     persistent: false
   });
-  
+
   event.completed();
 }
 
 // Gets the ID of the item and displays it in the info bar.
 function getItemID(event) {
-  // Limited to 150 characters max in the info bar, so 
+  // Limited to 150 characters max in the info bar, so
   // only grab the first 50 characters of the ID
   var itemID = Office.context.mailbox.item.itemId.substring(0, 50);
-  
+
   Office.context.mailbox.item.notificationMessages.addAsync("itemID", {
     type: "informationalMessage",
     icon: "red-icon-16",
     message: "Item ID: " + itemID,
     persistent: false
   });
-  
+
   event.completed();
 }
 
-// MIT License: 
- 
-// Permission is hereby granted, free of charge, to any person obtaining 
-// a copy of this software and associated documentation files (the 
-// ""Software""), to deal in the Software without restriction, including 
-// without limitation the rights to use, copy, modify, merge, publish, 
-// distribute, sublicense, and/or sell copies of the Software, and to 
-// permit persons to whom the Software is furnished to do so, subject to 
-// the following conditions: 
- 
-// The above copyright notice and this permission notice shall be 
-// included in all copies or substantial portions of the Software. 
- 
-// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, 
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// MIT License:
+
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// ""Software""), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
